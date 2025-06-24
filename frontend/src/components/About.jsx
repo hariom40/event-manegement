c951cd82209226a6744c37643ace532c3dcf10e5
hariom <nishaauradar@gmail.com>
2025-06-24 10:11:11 +0530

    first commit

---
+++ frontend/src/components/About.jsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="about container">
+      <h4>Events</h4>
+      <h2>ABOUT</h2>
+      <p>
+        King’s is a modern and user-friendly Event Management Web Application designed to 
+        streamline the organization, booking, and coordination of various events. 
+        Whether it's weddings, corporate functions, parties, or concerts, 
+        King’s empowers both event organizers and attendees with seamless interaction and
+         management tools. Every event deserves royal treatment 👑 — King’s brings elegance, order, and simplicity to event planning and participation.
+      </p>
+    </section>
+  );
+};
+
+export default About;
