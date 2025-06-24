e89b26f56d3def937433e3d2d501f035df5c7929
hariom <nishaauradar@gmail.com>
2025-06-24 10:42:28 +0530

    last commit

--- frontend/src/components/Contact.jsx
+++ frontend/src/components/Contact.jsx
@@ -12,7 +12,7 @@ const Contact = () => {
     e.preventDefault();
     await axios
       .post(
-        "http://localhost:4000/api/v1/message/send",
+        "https://event-manegement.onrender.com/api/v1/message/send",
         {
           name,
           email,
